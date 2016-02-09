d36bfc599232788fec94bf087482aec2c5922b51
Len Lyle <lenlyle@gmail.com>
2016-02-08 17:04:47 -0800

    more testing

--- protected/bootstrap.php
+++ protected/bootstrap.php
@@ -7,12 +7,8 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 
 if( preg_match('/dev\./', $_SERVER['SERVER_NAME'])) {
     define("IS_LIVE", false);
-    ini_set('display_errors', 1);
-    ini_set('log_errors', 0);
 } else {
     define("IS_LIVE", true);
-    ini_set('display_errors', 0);
-    ini_set('log_errors', 1);
 }
 
 /* timezone */
